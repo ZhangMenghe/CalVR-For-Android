e51d0eb33958631ade23b8598af28bb0ef8e321d
ZhangMenghe <zmh.leaves@gmail.com>
2018-09-04 13:42:25 -0700

    strokeDrawable

---
+++ myGLES/app/src/main/cpp/strokeDrawable.cpp
@@ -0,0 +1,55 @@
+//
+// Created by menghe on 7/28/2018.
+//
+
+#include "strokeDrawable.h"
+#include "arcore_utils.h"
+#include <stack>
+#include <GLES3/gl3.h>
+using namespace glm;
+
+void strokeDrawable::Initialization(AAssetManager *manager,std::stack<utils::glState>* stateStack) {
+    glDrawable::Initialization(manager, stateStack);
+    _shader_program = utils::CreateProgram("shaders/pointDrawable.vert", "shaders/point.frag", manager);
+
+    _attrib_vertices = glGetAttribLocation(_shader_program,"vPosition");
+    _uniform_arMVP_mat =  glGetUniformLocation(_shader_program, "uarMVP");
+
+    //Generate VAO and bind
+    glGenVertexArrays(1, &_VAO);
+    glBindVertexArray(_VAO);
+
+    //Generate VBO and bind
+    glGenBuffers(1, &_VBO);
+
+    //dynamic feed data
+    glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 2 * 4, nullptr, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(_attrib_vertices);
+    glVertexAttribPointer(_attrib_vertices, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glUseProgram(_shader_program);
+    glUniform4fv(glGetUniformLocation(_shader_program, "uColor"), 1, value_ptr(_default_color));
+    glUniform1f(glGetUniformLocation(_shader_program, "uPointSize"), _default_size);
+    glUseProgram(0);
+
+    checkGlError("Initialize point cloud renderer");
+}
+
+void strokeDrawable::drawImplementation(osg::RenderInfo &) const {
+    PushAllState();
+    glUseProgram(_shader_program);
+    glUniformMatrix4fv(_uniform_arMVP_mat, 1, GL_FALSE, value_ptr(_ar_mvp));
+
+    glBindVertexArray(_VAO);
+    glDrawArrays(GL_LINES, 0, 2);
+    glBindVertexArray(0);
+
+    glUseProgram(0);
+    checkGlError("Draw point cloud");
+
+    PopAllState();
+}
---
+++ myGLES/app/src/main/cpp/strokeDrawable.h
@@ -0,0 +1,46 @@
+//
+// Created by menghe on 7/28/2018.
+//
+
+#ifndef MYGLES_STROKEDRAWABLE_H
+#define MYGLES_STROKEDRAWABLE_H
+
+#include <osg/Drawable>
+#include "utils.h"
+#include "glDrawable.h"
+
+class strokeDrawable: public glDrawable {
+private:
+    GLuint _VAO;
+    GLuint _VBO;
+    GLuint _shader_program;
+
+    GLuint _attrib_vertices;
+    GLuint _uniform_arMVP_mat;
+
+    glm::vec4 _default_color= glm::vec4(1.0, 0.5, 0.0, 1.0);
+    float _default_size = 30.0f;
+    glm::mat4 _ar_mvp = glm::mat4();
+
+    float strokeData[8];
+
+public:
+    void setStrokePoints(const float *start_pos, const float* end_pos){
+        for(int i=0;i<3;i++){
+            strokeData[i] = start_pos[i];
+            strokeData[4 + i] = end_pos[i];
+        }
+        strokeData[3] = 1.0; strokeData[7] = 1.0f;
+
+        glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, 8 * sizeof(float), strokeData);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    void updateARMatrix(glm::mat4 mvp){
+        _ar_mvp = mvp;
+    }
+    void Initialization(AAssetManager * manager,std::stack<utils::glState>* stateStack);
+    void drawImplementation(osg::RenderInfo&) const;
+};
+
+#endif
