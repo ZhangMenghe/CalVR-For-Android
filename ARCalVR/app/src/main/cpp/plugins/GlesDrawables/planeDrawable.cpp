68db982b7280465df4d55ccf65e2213941501eec
ZhangMenghe <zmh.leaves@gmail.com>
2018-10-29 09:56:49 -0700

    SOLVED: glesDrawables with texture

--- ARCalVR/app/src/main/cpp/plugins/GlesDrawables/planeDrawable.cpp
+++ ARCalVR/app/src/main/cpp/plugins/GlesDrawables/planeDrawable.cpp
@@ -57,7 +57,6 @@ void planeDrawable::Initialization(){
     if(!_shader_program)
         LOGE("Failed to create shader program");
 
-    //Init attribs & uniforms
     _attrib_vertices = glGetAttribLocation(_shader_program, "vPosition");
     _uniform_model_mat = glGetUniformLocation(_shader_program, "uModel");
     _uniform_normal_vec = glGetUniformLocation(_shader_program, "uNormal");
@@ -68,42 +67,19 @@ void planeDrawable::Initialization(){
 
     std::string fhead(getenv("CALVR_RESOURCE_DIR"));
     osg::Image * img = osgDB::readImageFile(fhead+"textures/trigrid.png");
-    GLuint texture_map;
-    glGenTextures(1, &texture_map);
-    glBindTexture(GL_TEXTURE_2D, texture_map);
+    glGenTextures(1, &_texture_id);
+    glBindTexture(GL_TEXTURE_2D, _texture_id);
+    glTexImage2D(GL_TEXTURE_2D, 0,
+                 img->getPixelFormat(),
+                 img->s(),img->t(),0,
+                 img->getPixelFormat(), img->getDataType(),
+                 img->getDataPointer());
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->getsize, 200, 0, GL_RGBA, GL_UNSIGNED_BYTE, img->getDataPointer());
-    glTexImage2D(GL_TEXTURE_2D,0,0,0,img->s(),img->t(),img->getPixelFormat(),img->getDataType(),img->getDataPointer());
     glGenerateMipmap(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, 0);
-
-
-    //Generate VAO and bind
-    glGenVertexArrays(1, &_VAO);
-    glBindVertexArray(_VAO);
-
-    //Generate VBO and bind
-    glGenBuffers(1, &_VBO);
-
-    //dynamic feed data
-    glBindBuffer(GL_ARRAY_BUFFER, _VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * MAX_PLANE_VERTICES * 3, nullptr, GL_DYNAMIC_DRAW);
-    glEnableVertexAttribArray(_attrib_vertices);
-    glVertexAttribPointer(_attrib_vertices, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-    //Generate EBO
-    glGenBuffers(1, &_EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * MAX_PLANE_VERTICES*5, nullptr, GL_STATIC_DRAW);
-
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void planeDrawable::updateOnFrame(ArPlane* plane, osg::Vec3f color) {
@@ -116,13 +92,6 @@ void planeDrawable::updateOnFrame(ArPlane* plane, osg::Vec3f color) {
 
     _update_plane_vertices();
 
-    glBindBuffer(GL_ARRAY_BUFFER, _VBO);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(GLfloat)*_vertices.size()*3, _vertices.data());
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
-    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(GLushort) * _triangles.size(), _triangles.data());
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glUseProgram(_shader_program);
     glUniform3f(_uniform_color, color.x(), color.y(), color.z());
     glUseProgram(0);
@@ -131,9 +100,6 @@ void planeDrawable::updateOnFrame(ArPlane* plane, osg::Vec3f color) {
 void planeDrawable::drawImplementation(osg::RenderInfo&) const{
     cvr::glStateStack::instance()->PushAllState();
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
     glUseProgram(_shader_program);
     glDepthMask(GL_FALSE);
 
@@ -141,14 +107,16 @@ void planeDrawable::drawImplementation(osg::RenderInfo&) const{
     glUniform1i(_uniform_tex_sampler, 0);
     glBindTexture(GL_TEXTURE_2D, _texture_id);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glUniformMatrix4fv(_uniform_mvp_mat, 1, GL_FALSE, _view_proj_mat.ptr());
     glUniform3f(_uniform_normal_vec, _normal_vec.x(), _normal_vec.y(), _normal_vec.z());
     glUniformMatrix4fv(_uniform_model_mat, 1, GL_FALSE, _model_mat.ptr());
 
-    glBindVertexArray(_VAO);
-
-    glDrawElements(GL_TRIANGLES, _triangles.size(), GL_UNSIGNED_SHORT, nullptr);
-    glBindVertexArray(0);
+    glEnableVertexAttribArray(_attrib_vertices);
+    glVertexAttribPointer(_attrib_vertices, 3, GL_FLOAT, GL_FALSE, 0, _vertices.data());
+    glDrawElements(GL_TRIANGLES, _triangles.size(), GL_UNSIGNED_SHORT,  _triangles.data());
 
     glUseProgram(0);
     glDepthMask(GL_TRUE);
