ed9a303af450ee691ebbdf560484d76260832969
ZhangMenghe <zmh.leaves@gmail.com>
2018-11-07 14:44:14 -0800

    Update ARCalVR/app/src/main/java/com/samsung/arcalvr/MainActivity.java

--- ARCalVR/app/src/main/java/com/samsung/arcalvr/MainActivity.java
+++ ARCalVR/app/src/main/java/com/samsung/arcalvr/MainActivity.java
@@ -115,6 +115,22 @@ public class MainActivity extends AppCompatActivity
         track_bnt.addSubButton(findViewById(R.id.moveButton), -300f, -200f);
         track_bnt.addSubButton(findViewById(R.id.rotateButton), 300f, -200f);
 
+        FloatingActionButton trans_bnt = findViewById(R.id.moveButton);
+        trans_bnt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                JniInterface.JNIonSingleTouchDown(3,view.getX(), view.getY());
+            }
+        });
+
+        FloatingActionButton move_bnt = findViewById(R.id.rotateButton);
+        move_bnt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                JniInterface.JNIonSingleTouchDown(4, view.getX(), view.getY());
+            }
+        });
+
         FPSlabel = (TextView) findViewById(R.id.textViewFPS);
     }
     private void setupSurfaceView(){
